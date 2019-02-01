85e5a8de41e7bc3719bfefe4dcd44bcd1fe7911f
harry48225 <harrybest800@gmail.com>
2019-02-01 15:27:51 +0000

    Commented encrypter

--- Encrypter.java
+++ Encrypter.java
@@ -2,20 +2,25 @@ import java.util.Base64;
 import java.io.IOException;
 
 public class Encrypter {
-
+	/*
+		This class takes a string and then xors it with an encryption key.
+		It then encodes it with base 64. The class can also decrypt the strings.
+	*/
     public String encode(String s, String key)
 	{
+		/* Encodes a given string with the given key */
         return base64Encode(xorWithKey(s.getBytes(), key.getBytes()));
     }
 
     public String decode(String s, String key)
 	{
+		/* Decodes a given string with the given key */
         return new String(xorWithKey(base64Decode(s), key.getBytes()));
     }
 
     private byte[] xorWithKey(byte[] dataToEncode, byte[] key)
 	{
-		
+		/* Private method that actually does the xoring */
         byte[] encoded = new byte[dataToEncode.length];
 		
         for (int i = 0; i < dataToEncode.length; i++)
@@ -28,11 +33,13 @@ public class Encrypter {
 
     private byte[] base64Decode(String s)
 	{
+		/* Decodes base 64 back to UTF-8 */
 		return Base64.getDecoder().decode(s);
     }
 
     private String base64Encode(byte[] bytes)
 	{
+		/* Encodes UTF-8 into base 64 */
         return Base64.getEncoder().encodeToString(bytes).replaceAll("\\s", "");
     }
 }
