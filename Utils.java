4ff21902284859b3dd201b3d8c4c4635f90b42e5
Harry <harrybest800@gmail.com>
2019-02-03 22:17:46 +0000

    Commented utils

--- Utils.java
+++ Utils.java
@@ -1,11 +1,17 @@
 public class Utils
 {
-	public static boolean isBeforeInDictionary(String word1, String word2) // Returns true if word1 comes before word 2 in the dictionary
+	/* Contains useful functions */
+	
+	public static boolean isBeforeInDictionary(String word1, String word2)
 	{
+		/* Returns true if word1 comes before word 2 in the dictionary, checks recursively */
+		
+		// Covert the first letter to an integer
 		int word1FirstLetter = (int) word1.toLowerCase().toCharArray()[0];
 		int word2FirstLetter = (int) word2.toLowerCase().toCharArray()[0];
 		boolean beforeInDictionary = false;
 		
+		// Compare the letters
 		if (word1FirstLetter < word2FirstLetter)
 		{
 			beforeInDictionary = true;
@@ -16,6 +22,7 @@ public class Utils
 		}
 		else // They must be the same letter
 		{
+			// If either word is just 1 letter
 			if (word1.length() == 1 || word2.length() == 1)
 			{
 				beforeInDictionary = word1.length() < word2.length(); // If word 1 is shorter it comes before word 2 in the dictionary
