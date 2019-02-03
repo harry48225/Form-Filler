c45979c63fe0d851db67a29152f12c512a8eea29
Harry <harrybest800@gmail.com>
2019-02-03 21:03:36 +0000

    Comment JValidatedCombobox

--- components/JValidatedComboBox.java
+++ components/JValidatedComboBox.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class JValidatedComboBox extends JComboBox<String> implements JValidatedComponent, JSaveableComponent
 {
+	/* A combobox that can be saved */
+	
 	private String[] options;
 	
 	private final String ERROR_STRING = "Drop-down: Please select an option";
@@ -35,29 +37,37 @@ public class JValidatedComboBox extends JComboBox<String> implements JValidatedC
 	
 	private void addOptions()
 	{
+		/* Adds the options in the options array to the combobox */
+		
 		for (String option : options)
 		{
 			addItem(StringEscaper.unescape(option)); // Each option is escaped so it needs to be unescaped before being added
 		}
 	}
 	
-	public boolean validateAnswer()	// Validates the answer
+	public boolean validateAnswer()
 	{
+		/* Validates the user's entry */
+		
 		return (getSelectedIndex() != 0);
 	}
 	
 	public boolean presenceCheck()
 	{
+		/* Performs a presence check, in this case the presence check is the same as the normal validation */
 		return validateAnswer();
 	}
 	
 	public String getErrorString()
 	{
+		/* Returns the error string */
+		
 		return ERROR_STRING;
 	}
 	
 	private String getOptionsString()
 	{
+		/* Returns the options as a string separated by . */
 		String optionString = "";
 		
 		for (String option : options)
@@ -65,14 +75,14 @@ public class JValidatedComboBox extends JComboBox<String> implements JValidatedC
 			optionString += StringEscaper.escape(option) + ".";
 		}
 		
-		optionString = optionString.substring(0, optionString.length() - 1); // Trim off the trailing ,
+		optionString = optionString.substring(0, optionString.length() - 1); // Trim off the trailing .
 		
 		return optionString;
 	}
 	
 	public String toString()
 	{
-		// Returns a string that fully describes the combobox
+		/*  Returns a string that fully describes the combobox */
 		String asString = "combobox:";
 		
 		asString += getOptionsString();
