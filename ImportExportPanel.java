95912ac6279eb70145f6a359cfaf21c12280b411
harry48225 <harrybest800@gmail.com>
2019-02-04 14:24:03 +0000

    Commented ImportExportPanel

--- ImportExportPanel.java
+++ ImportExportPanel.java
@@ -13,12 +13,15 @@ import java.io.*;
 
 public class ImportExportPanel extends JPanel implements ActionListener, Helper
 {
-	
+	/* This class displays a panel that allows the user to 
+	import and export questions and forms form the system. */
+
 	private final String HELP_STRING = "This is the import export panel. You can use this panel to import and export questions or forms. To export, a question/form select it in the table and press export. To import, press import and select the question/form using the file browser.";
 	
 	private QuestionList questions;
 	private FormList forms;
 	
+	// Allows the user to select files
 	private JFileChooser fileChooser = new JFileChooser();
 	
 	private JButton importQuestionButton = new JButton("Import question");
@@ -45,17 +48,22 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 	
 	public String getHelpString()
 	{
+		/* Returns the help string */
 		return HELP_STRING;
 	}
 	
 	public void refreshTables()
 	{
+		/* Refreshes the form and question tables */
+		
 		questionSelectionPanel.refreshTable();
 		formSelectionPanel.refreshTable();
 	}
 	
 	private void prepareGUI()
 	{
+		/* Prepares the main panel and adds the question and form selection panels to it */
+		
 		this.setLayout(new GridLayout(0,2)); // 2 columns
 		
 		prepareImportExportQuestionsPanel();
@@ -67,8 +75,11 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 	
 	private void prepareImportExportFormPanel()
 	{
+		/* Prepares the import and export forms panel */
+		
 		importExportFormPanel.setLayout(new BoxLayout(importExportFormPanel, BoxLayout.PAGE_AXIS));
 		
+		// Create the forms border
 		TitledBorder border = BorderFactory.createTitledBorder(loweredetched, "Forms");
 		Font currentFont = border.getTitleFont();
 		border.setTitleFont(currentFont.deriveFont(Font.BOLD, 16)); // Make the font larger and bold
@@ -77,36 +88,42 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		
 		importExportFormPanel.setBorder(border); // Set the border
 		
+		// Add a 10px vertical space
 		importExportFormPanel.add(Box.createRigidArea(new Dimension(0,10)));
 		
 		// Add the import button
 		importFormButton.addActionListener(this);
-		importFormButton.setBackground(new Color(130,183,75));
-		importFormButton.setForeground(Color.WHITE);
-		importFormButton.setMaximumSize(new Dimension(100000, 40));
+		importFormButton.setBackground(new Color(130,183,75)); // Green
+		importFormButton.setForeground(Color.WHITE); // Make the text white
+		importFormButton.setMaximumSize(new Dimension(100000, 40)); // Very large width so that it fills all horizontal space
 		importFormButton.setMinimumSize(new Dimension(100000, 20));
 		importExportFormPanel.add(importFormButton);
 		
+		// Add a 10px vertical space
 		importExportFormPanel.add(Box.createRigidArea(new Dimension(0,10)));
 		
 		// Add the selection panel
 		formSelectionPanel = new SelectFormsPanel(forms,questions);
 		importExportFormPanel.add(formSelectionPanel);
 		
+		// Add a 10px vertical space
 		importExportFormPanel.add(Box.createRigidArea(new Dimension(0,10)));
 		
 		// Add the export button
 		exportFormButton.addActionListener(this);
-		exportFormButton.setBackground(new Color(169,196,235));
-		exportFormButton.setMaximumSize(new Dimension(100000, 40));
+		exportFormButton.setBackground(new Color(169,196,235)); // Blue
+		exportFormButton.setMaximumSize(new Dimension(100000, 40)); // Very large width so that it fills all horizontal space
 		exportFormButton.setMinimumSize(new Dimension(100000, 20));
 		importExportFormPanel.add(exportFormButton);
 	}
 	
 	private void prepareImportExportQuestionsPanel()
 	{
+		/* Prepares the panel that allows the user to import and export questions */
+		
 		importExportQuestionsPanel.setLayout(new BoxLayout(importExportQuestionsPanel, BoxLayout.PAGE_AXIS));
 		
+		// Create the question border
 		TitledBorder border = BorderFactory.createTitledBorder(loweredetched, "Questions");
 		Font currentFont = border.getTitleFont();
 		border.setTitleFont(currentFont.deriveFont(Font.BOLD, 16)); // Make the font larger and bold
@@ -115,28 +132,31 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		
 		importExportQuestionsPanel.setBorder(border); // Set the border
 		
+		
 		importExportQuestionsPanel.add(Box.createRigidArea(new Dimension(0,10)));
 		
 		// Add the import button
 		importQuestionButton.addActionListener(this);
-		importQuestionButton.setBackground(new Color(130,183,75));
-		importQuestionButton.setForeground(Color.WHITE);
-		importQuestionButton.setMaximumSize(new Dimension(100000, 40));
+		importQuestionButton.setBackground(new Color(130,183,75)); // Green
+		importQuestionButton.setForeground(Color.WHITE); // Make the text colour white
+		importQuestionButton.setMaximumSize(new Dimension(100000, 40)); // Very large width so that it fills all horizontal space
 		importQuestionButton.setMinimumSize(new Dimension(100000, 20));
 		importExportQuestionsPanel.add(importQuestionButton);
 		
+		// Create 10px of vertical space
 		importExportQuestionsPanel.add(Box.createRigidArea(new Dimension(0,10)));
 		
 		// Add the selection panel
 		questionSelectionPanel = new SelectQuestionsPanel(questions);
 		importExportQuestionsPanel.add(questionSelectionPanel);
 		
+		// Create 10px of vertical space
 		importExportQuestionsPanel.add(Box.createRigidArea(new Dimension(0,10)));
 		
 		// Add the export button
 		exportQuestionButton.addActionListener(this);
-		exportQuestionButton.setBackground(new Color(169,196,235));
-		exportQuestionButton.setMaximumSize(new Dimension(100000, 40));
+		exportQuestionButton.setBackground(new Color(169,196,235)); // Blue
+		exportQuestionButton.setMaximumSize(new Dimension(100000, 40)); // Very large width so that it fills all horizontal space
 		exportQuestionButton.setMinimumSize(new Dimension(100000, 20));
 		importExportQuestionsPanel.add(exportQuestionButton);
 	}
@@ -157,6 +177,7 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		}
 		else if (evt.getSource() == exportQuestionButton)
 		{
+			// Gets the id of the question that the user selected and allows them to export it
 			System.out.println("[INFO] <IMPORT_EXPORT_PANEL> exportQuestionButton pressed"); // Debug
 			String selectedQuestionID = questionSelectionPanel.getSelectedQuestionID();
 			if (selectedQuestionID != null)
@@ -166,6 +187,7 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		}
 		else if (evt.getSource() == exportFormButton)
 		{
+			// Gets the id of the form that the user selected and allows them to export it
 			System.out.println("[INFO] <IMPORT_EXPORT_PANEL> exportFormButton pressed"); // Debug
 			String selectedFormID = formSelectionPanel.getSelectedFormID();
 			if (selectedFormID != null)
@@ -177,14 +199,19 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 	
 	private void importQuestion()
 	{
+		/* Opens a file chooser and allows the user to select a question to 
+			import and then imports it */
 		System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Running importQuestion");
 		
+		// Create a new filter to only allow question files to be selected
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("Exported Questions", "question");
 		
 		fileChooser.setFileFilter(filter);
 		
+		// Open the file chooser
 		int result = fileChooser.showOpenDialog(this);
 		
+		// If they pressed the cancel option cancel the import process
 		if (result == JFileChooser.CANCEL_OPTION)
 		{
 			System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Importing question cancelled");
@@ -195,10 +222,10 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		
 		try
 		{
+			// Read the data from the file and import the question
 			String filePath = fileChooser.getSelectedFile().getAbsolutePath();
 			
 			BufferedReader br = new BufferedReader(new FileReader(filePath)); // Open the database file
-			
 			
 			questionImported = new ExportedQuestion(br.readLine()); // Read the ExportedQuestion from the file
 		}
@@ -228,14 +255,20 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 	
 	private void importForm()
 	{
+		/* Opens a file chooser and allows the user to select a form to 
+			import and then imports it */
+			
 		System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Running importForm");
 		
+		// Create a filter that only allows form files.
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("Exported Forms", "form");
 		
 		fileChooser.setFileFilter(filter);
 		
+		// Open the filter chooser
 		int result = fileChooser.showOpenDialog(this);
 		
+		// If they pressed cancel, cancel the process
 		if (result == JFileChooser.CANCEL_OPTION)
 		{
 			System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Importing form cancelled");
@@ -246,6 +279,7 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		
 		try
 		{
+			// Read the data from the file and import the form
 			String filePath = fileChooser.getSelectedFile().getAbsolutePath();
 			
 			BufferedReader br = new BufferedReader(new FileReader(filePath)); // Open the database file
@@ -295,6 +329,7 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 	
 	private void exportQuestion(String questionID)
 	{
+		/* Takes a question ID and exports the question that corresponds with it */
 		System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Running exportQuestion");
 		
 		Question q = questions.getQuestionByID(questionID); // Get the question
@@ -302,20 +337,27 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		
 		ExportedQuestion questionToExport = new ExportedQuestion(q, qP); // Create a new ExportedQuestion object
 		
+		// Create a new file with the correct name and make it automatically selected.
+		// This fills in the file name in the save dialog
 		fileChooser.setSelectedFile(new File(q.getTitle()));
 		
+		
+		// Only allow the user to save files with the extension form
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("Exported Questions", "question");
 		
 		fileChooser.setFileFilter(filter);
 		
+		// Get the user to choose a place to save
 		int result = fileChooser.showSaveDialog(this);
 		
+		// Cancel exporting the question if the user pressed cancel
 		if (result == JFileChooser.CANCEL_OPTION)
 		{
 			System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Exporting question cancelled");
 			return;
 		}
 		
+		// Get the file path that they selected
 		String filePath = fileChooser.getSelectedFile().getAbsolutePath();
 		
 		if (!filePath.endsWith(".question")) // If it doesn't end with question
@@ -323,6 +365,7 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 			filePath += ".question"; // Add the question file type
 		}
 		
+		// Export the question
 		try
 		{
 			FileWriter fw = new FileWriter(filePath);
@@ -337,22 +380,28 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		}
 		
 		System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Question exported!");
+		// Tell the user that the question has been exported
 		JOptionPane.showMessageDialog(this, "Question exported!");
 	}
 	
 	private void exportForm(String formID)
 	{
+		/* Takes a form ID and exports the form that corresponds with it */
 		System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Running exportForm");
 		Form form = forms.getFormByID(formID);
 		
+		// Only allow the user to save with the extension form
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("Exported Forms", "form");
 		
 		fileChooser.setFileFilter(filter);
 		
-		// Get the user to select a place to save the form
+		// Create a new file with the correct name and make it automatically selected.
+		// This fills in the file name in the save dialog
 		fileChooser.setSelectedFile(new File(form.getTitle()));
+		// Get the user to choose a place to save
 		int result = fileChooser.showSaveDialog(this);
 		
+		// Cancel exporting the form if the user pressed cancel
 		if (result == JFileChooser.CANCEL_OPTION)
 		{
 			System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Exporting form cancelled");
@@ -365,6 +414,8 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		ExportedQuestion[] exportedQuestions = new ExportedQuestion[questionIDs.length]; // Create an array to store the exported question objects
 		int nextExportedQuestionsLocation  = 0;
 		
+		// Fill the exportedQuestions array with ExportedQuestion objects
+		// corresponding to each question in the form.
 		for (int i = 0; i < questionIDs.length; i++) // For each question in the form
 		{
 			String questionID = questionIDs[i];
@@ -400,7 +451,7 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		
 		try
 		{
-			
+			// Write the exported form object to file
 			FileWriter fw = new FileWriter(filePath);
 			
 			fw.write(formToExport.toString()); // Write the form to file
@@ -413,6 +464,7 @@ public class ImportExportPanel extends JPanel implements ActionListener, Helper
 		}
 		
 		System.out.println("[INFO] <IMPORT_EXPORT_PANEL> Form exported!");
+		// Tell the user that the form was successfully exported
 		JOptionPane.showMessageDialog(this, "Form exported!");
 	}
 }
