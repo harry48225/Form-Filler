0bd2e4364d0c44a38166fbf057a326519ed9e56f
Harry <harrybest800@gmail.com>
2019-02-04 21:53:28 +0000

    Commented Report

--- Report.java
+++ Report.java
@@ -9,6 +9,8 @@ import java.time.*;
 
 public class Report extends JPanel
 {
+	/* This creates and formats a report that can be printed detailing a user's progress */
+	
 	private String[] tableHeaders = new String[] {"<html><center>Question<br>type</html>", "<html><center>Number of times<br>failed validation</html>", "<html><center>Average number<br>of attempts<br>needed to correct</html>", "<html><center>Average time<br>to complete<br>(seconds)</html>"}; // The headers for the table
 	private String[][] reportData; // The data about how the user is performing with the questions
 	private JTable reportTable;
@@ -27,7 +29,8 @@ public class Report extends JPanel
 	
 	private void prepareGUI()
 	{
-			
+		/* General prepare gui method */
+		
 		System.out.println("[INFO] <REPORT> Running prepareGUI");
 		
 		this.setLayout(new GridLayout(1,1));
@@ -39,6 +42,8 @@ public class Report extends JPanel
 	
 	private void prepareReportTable()
 	{
+		/* Prepares the report table */
+		
 		reportTable = new JTable(reportData, tableHeaders); // Create a new table
 		reportTableScrollPane = new JScrollPane(reportTable);
 		
@@ -61,10 +66,13 @@ public class Report extends JPanel
 		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
 		centerRenderer.setHorizontalAlignment( SwingConstants.CENTER );
 		
+		// Make the text in the data columns centered
 		reportTable.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
 		reportTable.getColumnModel().getColumn(2).setCellRenderer(centerRenderer);
 		reportTable.getColumnModel().getColumn(3).setCellRenderer(centerRenderer);
 		
+		// Setup the report table panel and make it have a white background
+		// so that it looks like a piece of paper.
 		reportTablePanel.setLayout(new BorderLayout());
 		reportTablePanel.setBackground(Color.WHITE);
 		reportTablePanel.add(reportTableScrollPane, BorderLayout.CENTER);
@@ -78,25 +86,33 @@ public class Report extends JPanel
 				}
 		});
 		
+		// Add the header panel to the top
 		reportTablePanel.add(createHeaderPanel(), BorderLayout.NORTH);
 	}
 	
 	private JPanel createHeaderPanel()
 	{
-		JPanel headerPanel = new JPanel();
-		headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.LINE_AXIS));
-		headerPanel.setBackground(Color.WHITE);
+		/* Prepares the panel that contains the user's username and the current date */
 		
-		headerPanel.add(Box.createHorizontalStrut(10));
+		JPanel headerPanel = new JPanel();
+		headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.LINE_AXIS)); // Horizontal box layout
+		headerPanel.setBackground(Color.WHITE); // Make it white to match the paper background
+		
+		headerPanel.add(Box.createHorizontalStrut(10)); // 10px horizontal spacing
+		
+		// Add a label with their username
 		headerPanel.add(new JLabel(username + "\'s report"));
-		headerPanel.add(Box.createHorizontalGlue());
+		
+		headerPanel.add(Box.createHorizontalGlue()); // Fills the horizontal space
+		
 		String rawToday = LocalDate.now().toString(); // Get today's date
 		// By default its yyyy-mm-dd
 		// We want dd-mm-yyyy
 		String[] todayArray = rawToday.split("-");
 		String today = todayArray[2] + "-" + todayArray[1] + "-" + todayArray[0];
 		headerPanel.add(new JLabel(today));
-		headerPanel.add(Box.createHorizontalStrut(10));
+		
+		headerPanel.add(Box.createHorizontalStrut(10)); // 10px horizontal padding
 		
 		return headerPanel;
 	}
