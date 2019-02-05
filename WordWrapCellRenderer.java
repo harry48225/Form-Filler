35d9987041cd44bac086e73a302968efee7cd787
Harry <harrybest800@gmail.com>
2019-02-05 19:36:30 +0000

    Commented WordWrapCellRenderer

--- WordWrapCellRenderer.java
+++ WordWrapCellRenderer.java
@@ -6,38 +6,45 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class WordWrapCellRenderer extends JTextPane implements TableCellRenderer 
+{
+	/* A cell renderer that wraps the words so that they can all be seen
+		regardless of the width of the column */
+		
+	public WordWrapCellRenderer()
 	{
-		public WordWrapCellRenderer()
-		{
-			//Font currentFont = this.getFont();
-			this.setFont(new Font("Monaco", Font.PLAIN, 13)); // Make the text larger 11
-		}
-		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) 
-		{
-			if (value != null) 
-			{
-				//System.out.println(value);
-				setText(value.toString());
-				setSize(table.getColumnModel().getColumn(column).getWidth(), getPreferredSize().height);
-				
-				
-				// Colour the cell the highlight colour if it's selected.
-				if(isSelected)
-				{
-					setBackground(table.getSelectionBackground());
-				}
-				else
-				{
-					setBackground(table.getBackground());
-				}
-				
-				// Make the text centered
-				StyledDocument doc = this.getStyledDocument();
-				SimpleAttributeSet center = new SimpleAttributeSet();
-				StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
-				doc.setParagraphAttributes(0, doc.getLength(), center, false);
-				
-			}
-			return this;
-		}
+		this.setFont(new Font("Monaco", Font.PLAIN, 13)); // Make the text larger 11
 	}
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+	{
+		/* This returns the styling and how the text in the cell should be displayed and handled */
+		
+		// If the value isn't null, i.e. there is something in the cell
+		if (value != null)
+		{
+			setText(value.toString());
+			
+			// Set the size of the cell to the correct size depending on its components
+			setSize(table.getColumnModel().getColumn(column).getWidth(), getPreferredSize().height);
+			
+			
+			// Colour the cell the highlight colour if it's selected.
+			if(isSelected)
+			{
+				setBackground(table.getSelectionBackground());
+			}
+			else
+			{
+				setBackground(table.getBackground());
+			}
+			
+			// Make the text centered
+			StyledDocument doc = this.getStyledDocument();
+			SimpleAttributeSet center = new SimpleAttributeSet();
+			StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+			doc.setParagraphAttributes(0, doc.getLength(), center, false);
+			
+		}
+		
+		return this;
+	}
+}
