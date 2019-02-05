32a2222f27befb52aa0a97a40da6c9ab5835551a
Harry <harrybest800@gmail.com>
2019-02-05 20:24:40 +0000

    Commented user

--- User.java
+++ User.java
@@ -2,17 +2,22 @@ import java.time.*;
 
 public class User
 {
+	/* All users in the system are stored as user objects.
+		A user object stores all of the data to do with a user */
+		
 	private String id;
 	
 	private String username;
 	private String password;
 	
 	// Sensitive info
+	// Store as encrypted by default as the database isn't initally decrypted
 	private String firstName = "encrypted";
 	private String lastName = "encrypted";
 	private String dateOfBirth = "encrypted";
 	private String phoneNumber = "encrypted";
 	
+	// Store whether the user is an admin user
 	private boolean admin;
 	
 	private String[] sessionsPresentAt;
@@ -21,19 +26,23 @@ public class User
 	
 	public User(String credentialString)
 	{
-	
+		/* Loads all of the information stored in a user saveString */
+		
+		// The data is delimited by commas
 		String[] splitData = credentialString.split(",");
 		
+		// Extract the data
 		id = splitData[0];
 		username = splitData[1];
 		password = splitData[2];
 		admin = Boolean.parseBoolean(splitData[3]);
 		
+		// Load the sessions present at array
 		sessionsPresentAt = new String[0];
 		
 		if (splitData.length > 4) // If the sessions present at is present
 		{
-			sessionsPresentAt = splitData[4].split("\\.");
+			sessionsPresentAt = splitData[4].split("\\."); // The sessions present at are delimited by . s
 		}
 	}
 	
@@ -41,6 +50,8 @@ public class User
 					String tempLastName, String tempDateOfBirth, String tempPhoneNumber, boolean tempAdmin,
 					String[] tempSessionsPresentAt, QuestionStatList tempQuestionStats)
 	{
+		/* Creates a new user with the attributes specified by the parameters */
+		
 		id = tempId;
 		username = tempUsername;
 		password = tempPassword;
@@ -58,6 +69,10 @@ public class User
 	
 	public void addSensitiveInformation(String sensitiveInfoString)
 	{
+		/* Adds the loaded sensitive info to the user.
+			This is called when the user database is decrypted */
+			
+		// The information is delimited by commas
 		String[] splitData = sensitiveInfoString.split(",");
 		
 		firstName = splitData[0];
@@ -68,11 +83,14 @@ public class User
 	
 	public boolean isAdmin()
 	{
+		/* Returns whether or not the user is an admin */
 		return admin;
 	}
 	
-	public void addPresentToday() // Adds today's date to the user's sessions present at array
+	public void addPresentToday()
 	{
+		/* Adds today's date to the user's sessions present at array */
+		
 		String rawToday = LocalDate.now().toString(); // Get today's date
 		// By default its yyyy-mm-dd
 		// We want dd-mm-yyyy
@@ -81,7 +99,6 @@ public class User
 		
 		if (sessionsPresentAt.length >= 1) // If they've been to at least one session
 		{
-			
 			String mostRecentSession = sessionsPresentAt[sessionsPresentAt.length - 1]; // Get the most recent session
 			
 			if (!mostRecentSession.equals(today)) // If the most recent session wasn't today
@@ -95,8 +112,10 @@ public class User
 		}
 	}
 	
-	public void addSession(String sessionDate) // Adds a date to the user's sessions array.
+	public void addSession(String sessionDate)
 	{
+		/* Adds a date to the user's sessions present at array. */
+		
 		String[] newSessions = new String[sessionsPresentAt.length + 1]; // Create a larger array to store the sessions
 		
 		for (int i = 0; i < sessionsPresentAt.length; i++)
@@ -109,6 +128,44 @@ public class User
 		sessionsPresentAt = newSessions;
 	}
 	
+	public String toString()
+	{
+		/* Returns a string containing all of the data about a user */
+		return id + "," + username + "," + password + "," + firstName + "," + lastName + "," + dateOfBirth + "," + phoneNumber + "," + admin + "," + sessionsToString();
+	}
+	
+	public String getCredentialString()
+	{
+		/* Returns just the username, password, admin status, and sessions the user has attended */
+		return id + "," + username + "," + password + "," + admin + "," + sessionsToString();
+	}
+	
+	public String getSensitiveString()
+	{
+		/* Returns the sensitive information stored about a user */
+		return id + "||" + firstName + "," + lastName + "," + dateOfBirth + "," + phoneNumber;	
+	}
+	
+	private String sessionsToString()
+	{
+		/* Converts the sessions array to string */
+		
+		String sessions = "";
+		
+		if (sessionsPresentAt.length > 0)
+		{
+			sessions = sessionsPresentAt[0]; // Start at the first session
+			
+			for (int i = 1; i < sessionsPresentAt.length; i++) // For every session (apart from the first)
+			{
+				sessions += "." + sessionsPresentAt[i]; // Append it to the string
+			}
+		}
+		
+		return sessions;
+	}
+	
+	/* These methods that follow are general setter and getter methods */
 	public String[] getSessionsAttended()
 	{
 		return sessionsPresentAt;
@@ -117,21 +174,6 @@ public class User
 	public String getID()
 	{
 		return id;
-	}
-	
-	public String toString()
-	{
-		return id + "," + username + "," + password + "," + firstName + "," + lastName + "," + dateOfBirth + "," + phoneNumber + "," + admin + "," + sessionsToString();
-	}
-	
-	public String getCredentialString()
-	{
-		return id + "," + username + "," + password + "," + admin + "," + sessionsToString();
-	}
-	
-	public String getSensitiveString()
-	{
-		return id + "||" + firstName + "," + lastName + "," + dateOfBirth + "," + phoneNumber;
 	}
 	
 	public String[] toStringArray()
@@ -217,21 +259,5 @@ public class User
 	public String getSessionsAttendedString()
 	{
 		return sessionsToString();
-	}
-	private String sessionsToString() // Converts the sessions array to string
-	{
-		String sessions = "";
-		
-		if (sessionsPresentAt.length > 0)
-		{
-			sessions = sessionsPresentAt[0]; // Start at the first one
-			
-			for (int i = 1; i < sessionsPresentAt.length; i++) // For every other session
-			{
-				sessions += "." + sessionsPresentAt[i];
-			}
-		}
-		
-		return sessions;
 	}
 }
